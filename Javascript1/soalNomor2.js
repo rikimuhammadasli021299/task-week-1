a0868d2639e0d15c4f2ef3e8d6ac1038936d90e1
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-10-19 18:51:46 +0700

    refactoring and rename variable in file soalNomor2.js

--- Javascript1/soalNomor2.js
+++ Javascript1/soalNomor2.js
@@ -1,32 +1,29 @@
-const hitungNilaiUN = (mtk, bahasaIndonesia, bahasaInggris, ipa, nilaiGrade) => {
-  if (mtk && bahasaIndonesia && bahasaInggris && ipa && typeof mtk === 'number' && typeof bahasaIndonesia === 'number' && typeof bahasaInggris === 'number' && typeof ipa === 'number') {
-    const rataRataNilai = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
-    nilaiGrade(rataRataNilai);
+const hitungGrade = (nilai) => {
+  let grade = '';
+  if (nilai <= 100 && nilai >= 90) {
+    return (grade = 'A');
+  } else if (nilai < 90 && nilai >= 80) {
+    return (grade = 'B');
+  } else if (nilai < 80 && nilai >= 70) {
+    return (grade = 'C');
+  } else if (nilai < 70 && nilai >= 60) {
+    return (grade = 'D');
   } else {
-    console.log('nilai tidak boleh ada yang kosong dan nilai harus bertipe number!');
+    return (grade = 'E');
   }
 };
 
-const hitungGarde = (nilai) => {
-  if (nilai > 100 || nilai < 0) {
-    console.log(`Skala nilai 0-100, total nilai yang diinputkan adalah ${nilai}`);
-  } else {
-    if (nilai <= 100 && nilai >= 90) {
-      const grade = 'A';
-      console.log(`Rata-Rata = ${nilai}\nGrade = ${grade}`);
-    } else if (nilai < 90 && nilai >= 80) {
-      const grade = 'B';
-      console.log(`Rata-Rata = ${nilai}\nGrade = ${grade}`);
-    } else if (nilai < 80 && nilai >= 70) {
-      const grade = 'C';
-      console.log(`Rata-Rata = ${nilai}\nGrade = ${grade}`);
-    } else if (nilai < 70 && nilai >= 60) {
-      const grade = 'D';
-      console.log(`Rata-Rata = ${nilai}\nGrade = ${grade}`);
-    } else if (nilai < 60 && nilai >= 0) {
-      const grade = 'E';
-      console.log(`Rata-Rata = ${nilai}\nGrade = ${grade}`);
+const hitungNilaiUN = (mtk, bahasaIndonesia, bahasaInggris, ipa, nilaiGrade) => {
+  if (mtk && bahasaIndonesia && bahasaInggris && ipa && typeof mtk === 'number' && typeof bahasaIndonesia === 'number' && typeof bahasaInggris === 'number' && typeof ipa === 'number') {
+    const rataRataNilai = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
+    if (rataRataNilai > 100 || rataRataNilai < 0) {
+      console.log(`Skala nilai 0-100, total nilai yang diinputkan adalah ${rataRataNilai}`);
+    } else {
+      const grade = nilaiGrade(rataRataNilai);
+      console.log(`Rata-Rata = ${rataRataNilai}\nGrade = ${grade}`);
     }
+  } else {
+    console.log('nilai tidak boleh ada yang kosong dan nilai harus bertipe number!');
   }
 };
 
@@ -35,4 +32,4 @@ const bahasaIndonesia = 90;
 const bahasaInggris = 89;
 const ipa = 69;
 
-hitungNilaiUN(mtk, bahasaIndonesia, bahasaInggris, ipa, hitungGarde);
+hitungNilaiUN(mtk, bahasaIndonesia, bahasaInggris, ipa, hitungGrade);
